0de8465fef8bad0b777a4ca6b4fabd3c9a2ac3a4
Keiko-M <morimoto.keiko@gmail.com>
2019-04-25 20:41:19 +0100

    added css to profile page

--- routes.php
+++ routes.php
@@ -26,7 +26,7 @@
 // Add an entry for each new controller and its actions
 $controllers = array('pages' => ['home', 'error'],
                     'post' => ['readAll','read','create','update','delete', 'search'],
-                    'controllerXXX' => ['actionYYY', 'actionZZZ'],
+//                    'controllerXXX' => ['actionYYY', 'actionZZZ'],
                     );
 
   // check that the requested controller and action are both allowed
--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -5,6 +5,7 @@ session_start();
 <html>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link href="../css/styles.css" rel="stylesheet" type="text/css"/>
     <script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
