35925d2549fb416c0a294d78ebe75a5ac5ed6283
Keiko-M <morimoto.keiko@gmail.com>
2019-04-25 20:14:58 +0100

    button color change

--- views/pages/login.php
+++ views/pages/login.php
@@ -17,7 +17,7 @@ session_start();
                 <div class="panel panel-login">
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-lg-12">
+                            <div class="col-lg-8">
                                 <div class="alert alert-danger" role="alert" id="error" style="display: none;">...</div>
                                 <form id="login-form" name="login_form" role="form" style="display: block;" method="post">
                                     <div class="form-group">
@@ -26,8 +26,8 @@ session_start();
                                     <div class="form-group">
                                         <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" required> 
                                     </div>
-                                    <div class="col-xs-12 form-group pull-right">     
-                                        <button type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-primary">
+                                    <div class="col-xs-6 form-group pull-right">     
+                                        <button type="submit" name="login-submit" id="login-submit" tabindex="3" class="form-control btn btn-warning">
                                         <span class="spinner"><i class="icon-spin icon-refresh" id="spinner"></i></span> Log In
                                         </button>
                                     </div>
