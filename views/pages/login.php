28110be7964819404a0506888c24aac66df63f2b
Keiko-M <morimoto.keiko@gmail.com>
2019-04-25 20:30:20 +0100

    added css link

--- views/pages/login.php
+++ views/pages/login.php
@@ -5,12 +5,16 @@ session_start();
 <html>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link href="../css/styles.css" rel="stylesheet" type="text/css"/>
+
     <script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
     <script type="text/javascript" src="../../controllers/loginout_controller.js"></script>
 <body>
-    <H1><?php print_r($_SESSION); ?></H1>
+<!--    <H1><?php print_r($_SESSION); ?></H1>-->
+    <H1>Please Login</H1>
+
     <div class="container">
         <div class="row box" id="login-box">
             <div class="col-md-6 col-md-offset-3">
